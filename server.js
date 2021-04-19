65fbf0f69e4e9acc3cc79de849cbf785a3652306
karolyi15 <karolyi15@hotmail.com>
2021-04-19 11:17:13 -0700

    profile page

--- server.js
+++ server.js
@@ -1,41 +1,69 @@
-// Requiring modules
-const express = require('express');
-const app = express();
-const mssql = require("mssql");
-  
-// Get request
-app.get('/', function (req, res) {
-  
-    // Config your database credential
-    const config = {
-        user: 'GK',
-        password: '123456',
-        server: 'localhost',
-        database: 'ControlPlanilla'
-    };
-  
-    // Connect to your database
-    mssql.connect(config, function (err) {
-  
-        // Create Request object to preform
-        // query operation
-        var request = new mssql.Request();
-  
-        // Query to the database and get the records
-        request.query('select * from Users',
-            function (err, records) {
-  
-                if (err) console.log(err)
-  
-                // Send records as a response
-                // to browser
-                console.log(records);
-                res.send(records);
-  
-            });
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+
+    var config = {  
+        server: '192.168.0.134',  //update me
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'GK', //update me
+                password: '123456'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'ControlPlanilla',  //update me
+            port: 1433,
+            trustServerCertificate: true
+        }
+    };  
+    
+    
+const connection = new Connection(config);
+
+connection.on('connect', (err) => {
+  if (err) {
+    console.log('Connection Failed');
+    throw err;
+  }
+
+  executeStatement();
+});
+
+connection.connect();
+
+function executeStatement() {
+  const request = new Request('select * from Users', (err, rowCount) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('DONE!');
+    connection.close();
+  });
+
+  // Emits a 'DoneInProc' event when completed.
+  request.on('row', (columns) => {
+    columns.forEach((column) => {
+      if (column.value === null) {
+        console.log('NULL');
+      } else {
+
+        console.log(column.value);
+        
+      }
     });
-});
-  
-var server = app.listen(1433, function () {
-    console.log('Server is listening at port 1433...');
-});
+  });
+
+  request.on('done', (rowCount) => {
+    console.log('Done is called!');
+  });
+
+  request.on('doneInProc', (rowCount, more) => {
+    console.log(rowCount + ' rows returned');
+  });
+
+  // In SQL Server 2000 you may need: connection.execSqlBatch(request);
+  connection.execSql(request);
+}
